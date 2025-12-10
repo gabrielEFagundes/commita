965c382d55a0dbe1c447be04b73340d05fa6f5cd
gabrielEFagundes <gabriel_e_fagundes@estudante.sesisenai.org.br>
2025-12-10 19:41:40 -0300

    :art: chore:  Adding various ANSI colors

--- tools/exception_handler.c
+++ tools/exception_handler.c
@@ -4,53 +4,31 @@
 #include "headers/exceptions.h"
 
 void exception_handler(char *err){
-    // here's where the I'll have loads of fun ;P
+    // here's where I'll have loads of fun ;P
 }
 
 void parse_exception(GitExceptions exc){
     switch(exc){
-        case NO_REPO:
-            // todo
-            break;
+        case NO_REPO: return "";
 
-        case MISSING_REMOTE_CHANGES:
-            // todo
-            break;
+        case MISSING_REMOTE_CHANGES: return "";
 
-        case NOT_SPECIFIED_REMOTE_BRANCH:
-            // todo
-            break;
+        case NOT_SPECIFIED_REMOTE_BRANCH: return "";
 
-        case OVERWRITING_CHANGES:
-            // todo
-            break;
+        case OVERWRITING_CHANGES: return "";
 
-        case COMMIT_AFTER_MERGE:
-            // todo
-            break;
+        case COMMIT_AFTER_MERGE: return "";
 
-        case MISSING_PERMISSIONS:
-            // todo
-            break;
+        case MISSING_PERMISSIONS: return "";
 
-        case INTERRUPTED_OPERATION:
-            // todo
-            break;
+        case INTERRUPTED_OPERATION: return "";
 
-        case NON_EXISTING_BRANCH:
-            // todo
-            break;
+        case NON_EXISTING_BRANCH: return "";
 
-        case INVALID_REMOTE_CREDENTIALS:
-            // todo
-            break;
+        case INVALID_REMOTE_CREDENTIALS: return "";
 
-        case UNRELATED_HISTORIES:
-            // todo
-            break;
+        case UNRELATED_HISTORIES: return "";
 
-        default:
-            // should be if the error is not found or if exc is NULL
-            break;
+        default: return ""; // should be if the error is not found or if exc is NULL
     }
 }
